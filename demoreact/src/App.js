c99b4633546f107ba32dfe03928b1cfdcd6c0bac
Abril Ureña <107883174+abrilurena@users.noreply.github.com>
2023-12-07 16:09:02 +0000

    Commit made from VS Code Azure Static Web Apps

--- demoreact/src/App.js
+++ demoreact/src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
 import { motion } from "framer-motion";
 
-/*Changes the text color and animates it */
-
 function App() {
   return (
-    <motion.div 
+    <motion.div
     className='box'
-    animate={{scale: [1, 2, 2, 1, 1],
-      rotate: [0, 0, 270, 270, 0],
-      borderRadius: ["20%", "20%", "50%", "50%", "20%"]}} 
-    
-  ></motion.div>
+    animate={{
+      scale: [1, 2, 2, 1],
+      rotate: [0, 0, 270, 270],
+      borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+      }}>
+    </motion.div>
   );
 }
 
